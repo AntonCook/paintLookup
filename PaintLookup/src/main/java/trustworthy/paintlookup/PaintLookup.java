31a288a89bec6a6264e67470177bcf4757eef039
moomoojuice2606 <anton.cook2053@gmail.com>
2024-09-10 08:05:04 -0500

    Adding all Project Files

---
+++ PaintLookup/src/main/java/trustworthy/paintlookup/PaintLookup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package trustworthy.paintlookup;
+
+/**
+ *
+ * @author dave
+ */
+public class PaintLookup {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
